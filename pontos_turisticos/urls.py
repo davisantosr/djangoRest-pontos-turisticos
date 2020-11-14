e3340ceca329d5a420db1fa574de72e37a1c884a
davisantosr <davi-rocha2@hotmail.com>
2020-11-14 17:03:56 -0300

    implementing endpoint|enderecos

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,12 +19,14 @@ from rest_framework import routers
 
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecosViewSet)
 
 
 urlpatterns = [
