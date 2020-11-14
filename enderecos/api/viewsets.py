ee7e9031fded6b27fbc6c48d0754d78b11daf4c2
davisantosr <davi-rocha2@hotmail.com>
2020-11-14 17:25:49 -0300

    implementing endpoint|comentarios

--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -4,6 +4,6 @@ from enderecos.models import Endereco
 from .serializers import EnderecoSerializer
 
 
-class EnderecosViewSet(ModelViewSet):
+class EnderecoViewSet(ModelViewSet):
   queryset = Endereco.objects.all()
   serializer_class = EnderecoSerializer
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -18,15 +18,18 @@ from django.urls import path, include
 from rest_framework import routers
 
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
-from enderecos.api.viewsets import EnderecosViewSet
+from atracoes.api.viewsets import AtracaoViewSet
+from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
-router.register(r'enderecos', EnderecosViewSet)
+router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+
 
 
 urlpatterns = [
