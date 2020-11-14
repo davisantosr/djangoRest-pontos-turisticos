b3a74b0af827681c3c40d0f76be16f8bcfee045d
davisantosr <davi-rocha2@hotmail.com>
2020-11-14 18:44:59 -0300

    overriding CRUD from viewsets|destroy(DELETE)

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status 
 
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -19,3 +20,7 @@ class PontoTuristicoViewSet(ModelViewSet):
 
 # def create(self, request, *args, **kwargs):
 #   return Response({'Hello': request.data['user']}) #Na requisição é passado um objeto com chave 'user'
+
+  def destroy(self, request, *args, **kwargs):
+    print(request)
+    return Response('ok', status=status.HTTP_200_OK)
