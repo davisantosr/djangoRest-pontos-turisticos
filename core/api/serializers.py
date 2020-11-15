249ae289f2d0ed9103ab8cf881da88c9dd28d5b5
davisantosr <davi-rocha2@hotmail.com>
2020-11-15 17:44:22 -0300

    nested relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.models import PontoTuristico
-
+from atracoes.api.serializers import AtracaoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+  atracoes = AtracaoSerializer(many=True,)
+
   class Meta:
     model = PontoTuristico
-    fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',)
+    fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes',)
