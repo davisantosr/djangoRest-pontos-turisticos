4b18be84081b00f83bc0800e0354292515c6f041
davisantosr <davi-rocha2@hotmail.com>
2020-11-15 15:44:19 -0300

    using token authentication

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -22,6 +22,9 @@ from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
 
+#Authentication
+from rest_framework.authtoken import views
+
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
@@ -41,4 +44,7 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+
+    path('auth/', views.obtain_auth_token)
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
